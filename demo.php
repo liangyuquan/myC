b3b9575a158964c9c095689e30f76ce9c544d1f4
liangyuquan <363016588@qq.com>
2019-03-19 11:48:10 +0800

    composer init

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administration
+ * Date: 2019/3/19
+ * Time: 11:27
+ */
+
+require_once './vendor/autoload.php';
+use app\start\Tool;
+
+Tool::index();
---
+++ src/app/start/Tool.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\start;
+
+class Tool
+{
+    public function __construct()
+    {
+    }
+
+    public static function index()
+    {
+        echo "welcome quan work";
+    }
+}
